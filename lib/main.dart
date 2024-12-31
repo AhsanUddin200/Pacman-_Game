b854b224f39a7e7b4bc2705c4d104be051f3c161
Ahsan <ahsanuddin160@gmail.com>
2024-12-31 12:44:33 +0500

    Pacman Game

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 enum Direction { up, down, left, right }
 
@@ -258,23 +259,75 @@ class _PacmanGameState extends State<PacmanGame> {
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Game Over!'),
+          backgroundColor: Color(0xFF000B45),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: Colors.blue[900]!, width: 3),
+          ),
+          title: Text(
+            'GAME OVER!',
+            style: GoogleFonts.pressStart2p(
+              textStyle: TextStyle(
+                color: Colors.red,
+                fontSize: 24,
+              ),
+            ),
+            textAlign: TextAlign.center,
+          ),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text('Final Score: $score'),
-              SizedBox(height: 10),
-              Text('Dots collected: $score'),
-              Text('Dots remaining: ${food.length}'),
+              Text(
+                'Final Score: $score',
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Dots collected: $score',
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'Dots remaining: ${food.length}',
+                style: GoogleFonts.pressStart2p(
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
             ],
           ),
           actions: <Widget>[
-            TextButton(
-              child: Text('Play Again'),
-              onPressed: () {
-                Navigator.pop(context);
-                resetGame();
-              },
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  resetGame();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[700],
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text(
+                  'PLAY AGAIN',
+                  style: GoogleFonts.pressStart2p(
+                    textStyle: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
             ),
           ],
         );
@@ -315,134 +368,261 @@ class _PacmanGameState extends State<PacmanGame> {
         }
       },
       child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onVerticalDragUpdate: (details) {
-                  if (details.delta.dy > 0) {
-                    moveInDirection(Direction.down);
-                  } else if (details.delta.dy < 0) {
-                    moveInDirection(Direction.up);
-                  }
-                },
-                onHorizontalDragUpdate: (details) {
-                  if (details.delta.dx > 0) {
-                    moveInDirection(Direction.right);
-                  } else if (details.delta.dx < 0) {
-                    moveInDirection(Direction.left);
-                  }
-                },
-                child: GridView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: numberOfSquares,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: numberInRow,
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    if (player == index) {
-                      return Transform.rotate(
-                        angle: pacmanDirection == Direction.right
-                            ? 0
-                            : pacmanDirection == Direction.down
-                                ? pi / 2
-                                : pacmanDirection == Direction.left
-                                    ? pi
-                                    : -pi / 2,
-                        child: Padding(
-                          padding: EdgeInsets.all(4),
-                          child: CustomPaint(
-                            painter: PacmanPainter(mouthClosed),
-                          ),
-                        ),
-                      );
-                    } else if (ghost == index) {
-                      return Padding(
-                        padding: EdgeInsets.all(4),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      );
-                    } else if (barriers.contains(index)) {
-                      return Container(
-                        color: Colors.blue[900],
-                      );
-                    } else if (food.contains(index)) {
-                      return Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.white.withOpacity(0.5),
-                                spreadRadius: 1,
-                                blurRadius: 3,
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    } else {
-                      return Container(
-                        color: Colors.black,
-                      );
-                    }
-                  },
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Score: $score',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            gamePaused = !gamePaused;
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue[900],
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                        ),
-                        child: Text(
-                          gamePaused ? 'PLAY' : 'PAUSE',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                        onPressed: resetGame,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.red,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                        ),
-                        child: Text(
-                          'RESET',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                    ],
+        backgroundColor: Color(0xFF000B45),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            'PACMAN',
+            style: GoogleFonts.pressStart2p(
+              textStyle: TextStyle(
+                color: Colors.yellow,
+                fontSize: 28,
+                shadows: [
+                  Shadow(
+                    color: Colors.yellow.withOpacity(0.5),
+                    offset: Offset(0, 2),
+                    blurRadius: 4,
                   ),
                 ],
               ),
             ),
-          ],
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF000B45),
+                Color(0xFF000000),
+              ],
+            ),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue[900]!, width: 3),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blue[900]!.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(17),
+                    child: GestureDetector(
+                      onVerticalDragUpdate: (details) {
+                        if (details.delta.dy > 0) {
+                          moveInDirection(Direction.down);
+                        } else if (details.delta.dy < 0) {
+                          moveInDirection(Direction.up);
+                        }
+                      },
+                      onHorizontalDragUpdate: (details) {
+                        if (details.delta.dx > 0) {
+                          moveInDirection(Direction.right);
+                        } else if (details.delta.dx < 0) {
+                          moveInDirection(Direction.left);
+                        }
+                      },
+                      child: GridView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: numberOfSquares,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: numberInRow,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          if (player == index) {
+                            return Transform.rotate(
+                              angle: pacmanDirection == Direction.right
+                                  ? 0
+                                  : pacmanDirection == Direction.down
+                                      ? pi / 2
+                                      : pacmanDirection == Direction.left
+                                          ? pi
+                                          : -pi / 2,
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: CustomPaint(
+                                  painter: PacmanPainter(mouthClosed),
+                                ),
+                              ),
+                            );
+                          } else if (ghost == index) {
+                            return Padding(
+                              padding: EdgeInsets.all(4),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                            );
+                          } else if (barriers.contains(index)) {
+                            return Container(
+                              color: Colors.blue[900],
+                            );
+                          } else if (food.contains(index)) {
+                            return Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.white.withOpacity(0.5),
+                                      spreadRadius: 1,
+                                      blurRadius: 3,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          } else {
+                            return Container(
+                              color: Colors.black,
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                decoration: BoxDecoration(
+                  color: Color(0xFF000B45),
+                  border: Border(
+                    top: BorderSide(color: Colors.blue[900]!, width: 3),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.blue[900]!,
+                            Colors.blue[800]!,
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                        border: Border.all(
+                          color: Colors.blue[300]!,
+                          width: 2,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'Score: $score',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            Shadow(
+                              color: Colors.black.withOpacity(0.5),
+                              offset: Offset(1, 1),
+                              blurRadius: 2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              gamePaused = !gamePaused;
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: gamePaused
+                                ? Colors.green[600]
+                                : Colors.green[700],
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 25, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                              side: BorderSide(
+                                color: Colors.green[300]!,
+                                width: 2,
+                              ),
+                            ),
+                            elevation: 5,
+                          ),
+                          child: Text(
+                            gamePaused ? 'PLAY' : 'PAUSE',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  color: Colors.black.withOpacity(0.5),
+                                  offset: Offset(1, 1),
+                                  blurRadius: 2,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.yellow[700]!,
+                                Colors.yellow[600]!
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Colors.yellow[300]!,
+                              width: 2,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.warning,
+                              color: Colors.black87,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
